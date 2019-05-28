d78dac91f0aba197289467fe76388ba7c23c7c83
Klesfot <mukhamediev.l@gmail.com>
2019-05-28 12:19:29 +0300

    DelAll

---
+++ L9/P6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Threading;
+
+namespace L9
+{
+    class P6
+    {
+        public static void Task1()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(DateTime.Now.Hour + " " + DateTime.Now.Minute + " " + DateTime.Now.Second);
+            }
+        }
+    }
+}
